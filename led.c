d9a6b0535c4e358de70b55e7fec689c7ad42a104
Andy Holmes <tho78@uclive.ac.nz>
2020-10-11 20:11:24 +1300

    Complete led.c

--- led.c
+++ led.c
@@ -1,7 +1,7 @@
-/** @file   beep1.c
-    @author Tawatchai Holmes (Andy), Zhedong Cao (Barry)
-    @date   15 OCT 2020
-    @Descr:  Makefile for game
+/** @file:      led.c
+    @author:    Tawatchai Holmes (Andy), Zhedong Cao (Barry)
+    @date:      15 OCT 2020
+    @Descr:     Led function use in game.c
 */
 
 #include "pio.h"
