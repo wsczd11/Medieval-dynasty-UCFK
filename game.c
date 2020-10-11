1444138465eeeaf370ac1e08787d6e1c97a83fd8
Andy Holmes <tho78@uclive.ac.nz>
2020-10-11 18:42:26 +1300

    Add text and change variable plus function

--- game.c
+++ game.c
@@ -7,8 +7,7 @@
 #include "led.h"
 
 
-
-/** Define PIO pins driving LED matrix rows.  */
+/* Define PIO pins driving LED matrix rows.  */
 static const pio_t rows[] = {
     LEDMAT_ROW1_PIO, LEDMAT_ROW2_PIO, LEDMAT_ROW3_PIO,
     LEDMAT_ROW4_PIO, LEDMAT_ROW5_PIO, LEDMAT_ROW6_PIO,
@@ -16,37 +15,42 @@ static const pio_t rows[] = {
 };
 
 
-/** Define PIO pins driving LED matrix columns.  */
+/* Define PIO pins driving LED matrix columns.  */
 static const pio_t cols[] = {
     LEDMAT_COL1_PIO, LEDMAT_COL2_PIO, LEDMAT_COL3_PIO,
     LEDMAT_COL4_PIO, LEDMAT_COL5_PIO
 };
 
-//Different choices
+
+/* Different choices for the game */
 static const uint8_t bitmap[] = {
-    0x00, 0x10, 0x3E, 0x10, 0x00,      //sword
-    0x00, 0x1E, 0x3E, 0x1E, 0x00,      //shield
-    0x00, 0x28, 0x1C, 0x28, 0x00,      //human
-    0x04, 0x04, 0x0C, 0x04, 0x1C,      //gun
-    0x00, 0x38, 0x3E, 0x3A, 0x00       //grenade
+    0x00, 0x10, 0x3E, 0x10, 0x00,      //Sword
+    0x00, 0x1E, 0x3E, 0x1E, 0x00,      //Shield
+    0x00, 0x28, 0x1C, 0x28, 0x00,      //Human
+    0x04, 0x04, 0x0C, 0x04, 0x1C,      //Gun
+    0x00, 0x38, 0x3E, 0x3A, 0x00       //Grenade
 };
 
 
-//Display the letter 'W'
+/* Display the letter 'W' */
 static const uint8_t winner_screen[] = {
     0x0E, 0x30, 0x0E, 0x30, 0x0E
 };
 
-//Display the letter 'L'
+
+/* Display the letter 'L' */
 static const uint8_t loser_screen[] = {
     0x00, 0x3E, 0x20, 0x20, 0x00
 };
 
-//Display the letter 'S'
+
+/* Display the letter 'S' */
 static const uint8_t equal_screen[] = {
     0x00, 0x24, 0x2A, 0x12, 0x00
 };
 
+
+/* Initilise the LED Metrics */
 static void inf(void)
 {
     for (int i = 0; i < 5; i++) {
@@ -57,7 +61,9 @@ static void inf(void)
     }
 }
 
-static void display_column (uint8_t row_pattern, uint8_t current_column)
+
+/* Display the LED */
+static void display_column(uint8_t row_pattern, uint8_t current_column)
 {
     for (int i = 0; i < 5; i++) {
         pio_output_high(cols[i]);
@@ -76,35 +82,38 @@ static void display_column (uint8_t row_pattern, uint8_t current_column)
     }
 }
 
-// If player1_choice win, return 0, else return 1. (Compare)
-static int compare_choice(uint8_t player1_choice, uint8_t player2_choice)
+
+/* Comparing the 2 different choices from each of the player
+ * If Player1 win, it will return 0 and if Player1 lose
+ * it will return 1 */
+static int compare_choice(uint8_t Player_1, uint8_t Player_2)
 {
-    if (player1_choice == 0) {
-        if (player2_choice == 2 || player2_choice == 3) {
+    if (Player_1 == 0) {
+        if (Player_2 == 2 || Player_2 == 3) {
             return 0;
         } else {
             return 1;
         }
-    } else if (player1_choice == 1) {
-        if (player2_choice == 0 || player2_choice == 4) {
+    } else if (Player_1 == 1) {
+        if (Player_2 == 0 || Player_2 == 4) {
             return 0;
         } else {
             return 1;
         }
-    } else if (player1_choice == 2) {
-        if (player2_choice == 1 || player2_choice == 4) {
+    } else if (Player_1 == 2) {
+        if (Player_2 == 1 || Player_2 == 4) {
             return 0;
         } else {
             return 1;
         }
-    } else if (player1_choice == 3) {
-        if (player2_choice == 1 || player2_choice == 2) {
+    } else if (Player_1 == 3) {
+        if (Player_2 == 1 || Player_2 == 2) {
             return 0;
         } else {
             return 1;
         }
     } else {
-        if (player2_choice == 0 || player2_choice == 3) {
+        if (Player_2 == 0 || Player_2 == 3) {
             return 0;
         } else {
             return 1;
@@ -112,92 +121,97 @@ static int compare_choice(uint8_t player1_choice, uint8_t player2_choice)
     }
 }
 
-//Display 'W' on Winner screen
-static void winner_display(uint8_t current_choice, uint8_t current_column)
+
+/* Display 'W' on Winner screen if the player wins */
+static void winner_display(uint8_t Player_1, uint8_t current_column)
 {
     while (1) {
         pacer_wait();
-        navswitch_update ();
-        display_column (winner_screen[current_column], current_column);
+        navswitch_update();
+        display_column(winner_screen[current_column], current_column);
         current_column++;
         if (current_column == 5) {
             current_column = 0;
         }
         if (button_pressed_p()) {
-            ir_uart_putc (current_choice + 1);
+            ir_uart_putc (Player_1 + 1);
         }
-        if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
+        if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
             break;
         }
     }
 }
 
 
-//Display 'L' on Loser screen
-static void loser_display(uint8_t current_choice, uint8_t current_column)
+/* Display 'L' on Loser screen if the player loss */
+static void loser_display(uint8_t Player_1, uint8_t current_column)
 {
     while (1) {
         pacer_wait();
-        navswitch_update ();
-        display_column (loser_screen[current_column], current_column);
+        navswitch_update();
+        display_column(loser_screen[current_column], current_column);
         current_column++;
         if (current_column == 5) {
             current_column = 0;
         }
         if (button_pressed_p()) {
-            ir_uart_putc (current_choice + 1);
+            ir_uart_putc (Player_1 + 1);
         }
-        if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
+        if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
             break;
         }
     }
 }
 
 
-//Display 'S' on both of the screen when they have the same choice
-static void equal_display(uint8_t current_choice, uint8_t current_column)
+/* Display 'S' on both of the screen when both of the player
+ * have the same choice */
+static void equal_display(uint8_t Player_1, uint8_t current_column)
 {
     while (1) {
         pacer_wait();
-        navswitch_update ();
+        navswitch_update();
         display_column (equal_screen[current_column], current_column);
         current_column++;
         if (current_column == 5) {
             current_column = 0;
         }
         if (button_pressed_p()) {
-            ir_uart_putc (current_choice + 1);
+            ir_uart_putc (Player_1 + 1);
         }
-        if (navswitch_push_event_p (NAVSWITCH_PUSH)) {
+        if (navswitch_push_event_p(NAVSWITCH_PUSH)) {
             break;
         }
     }
 }
 
-static int job(uint8_t current_choice, uint8_t current_column)
+
+/* Compare both of the input that both of the player have chose and then
+ * determine which of the player wins/lose or tie by the if's statement and
+ * determine the display winner_display, loser_display or equal_display by the
+ * player's choice */
+static int determine_outcome(uint8_t Player_1, uint8_t current_column)
 {
-    uint8_t partner_choice;
+    uint8_t Player_2;
     inf();
     while(1) {
         pacer_wait();
 
         if (button_pressed_p()) {
-            ir_uart_putc (current_choice + 1);
+            ir_uart_putc (Player_1 + 1); //Encode
         }
         if (ir_uart_read_ready_p()) {
-            partner_choice = ir_uart_getc() - 1;
-            if (partner_choice != 0 && partner_choice != 1 && partner_choice != 2 && partner_choice != 3 && partner_choice != 4) {
+            Player_2 = ir_uart_getc() - 1; //Decode
+            if (Player_2 != 0 && Player_2 != 1 && Player_2 != 2 && Player_2 != 3 && Player_2 != 4) { //Check index
                 continue;
-            } else if (current_choice == partner_choice) {
-                equal_display(current_choice, current_column);
+            } else if (Player_1 == Player_2) {
+                equal_display(Player_1, current_column);
                 return 0;
-            } else if (compare_choice(current_choice, partner_choice) == 0) {
-                //current_choice win
-                winner_display(current_choice, current_column);
+            } else if (compare_choice(Player_1, Player_2) == 0) {
+                winner_display(Player_1, current_column); //Player_1 win
                 return 1;
             } else {
-                //partner_choice win
-                loser_display(current_choice, current_column);
+                loser_display(Player_1, current_column); //Player_2 win
                 return 0;
             }
         }
@@ -208,9 +222,10 @@ static int job(uint8_t current_choice, uint8_t current_column)
     }
 }
 
+
 int main (void)
 {
-    uint8_t current_choice = 0;
+    uint8_t Player_1 = 0;
     uint8_t current_column = 0;
     uint16_t number = 0;
 
@@ -220,39 +235,36 @@ int main (void)
     button_init ();
     ir_uart_init ();
     led_init ();
-
-    /* TODO: Initialise LED matrix pins.  */
     inf();
 
     while (1) {
-        pacer_wait ();
 
+        pacer_wait ();
         navswitch_update ();
 
-        if (navswitch_push_event_p (NAVSWITCH_EAST)) {
-            if (current_choice == 4) {
-                current_choice = 0;
+        if (navswitch_push_event_p (NAVSWITCH_EAST)) { //Selection
+            if (Player_1 == 4) {
+                Player_1 = 0;
             } else {
-                current_choice++;
+                Player_1++;
             }
         }
-        if (navswitch_push_event_p (NAVSWITCH_WEST)) {
-            if (current_choice == 0) {
-                current_choice = 4;
+        if (navswitch_push_event_p (NAVSWITCH_WEST)) { //Selection
+            if (Player_1 == 0) {
+                Player_1 = 4;
             } else {
-                current_choice--;
+                Player_1--;
             }
         }
 
-        display_column (bitmap[current_choice * 5 + current_column], current_column);
-
+        display_column(bitmap[Player_1 * 5 + current_column], current_column);
         current_column++;
 
         if (current_column == 5) {
             current_column = 0;
         }
-        if (navswitch_push_event_p (NAVSWITCH_NORTH)) {
-            number += job(current_choice, current_column);
+        if (navswitch_push_event_p (NAVSWITCH_NORTH)) { //Comparision
+            number += determine_outcome(Player_1, current_column);
         }
         if (number == 3){
             ir_uart_putc ('A');
@@ -260,7 +272,7 @@ int main (void)
             break;
         }
         if (ir_uart_read_ready_p()) {
-            if (ir_uart_getc() == 'A'){
+            if (ir_uart_getc() == 'A'){ //Turn LED off for player who loss
                 led_off();
                 inf();
                 break;
