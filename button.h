6a50b9c0941f8af89447a1282cb31c6934936e06
Andy Holmes <tho78@uclive.ac.nz>
2020-10-11 20:07:20 +1300

    Complete button.h

--- button.h
+++ button.h
@@ -1,18 +1,18 @@
-/** @file   beep1.c
-    @author Tawatchai Holmes (Andy), Zhedong Cao (Barry)
-    @date   15 OCT 2020
-    @Descr:  Makefile for game
+/** @file:      button.h
+    @author:    Tawatchai Holmes (Andy), Zhedong Cao (Barry)
+    @date:      15 OCT 2020
+    @Descr:     Button functions in .h format
 */
 
 #ifndef BUTTON_H
 #define BUTTON_H
 
 
-/* Return non-zero if button pressed.  */
+/* Return non-zero if button pressed. */
 int button_pressed_p (void);
 
 
-/* Initialise button1.  */
+/* Initialise button1. */
 void button_init (void);
 
 #endif
