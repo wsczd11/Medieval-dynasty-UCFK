156c6839eb64f4191d3cb3a4657b572038f53606
Andy Holmes <tho78@uclive.ac.nz>
2020-10-11 20:10:15 +1300

    Complete game.c

--- game.c
+++ game.c
@@ -2,6 +2,25 @@
     @author:    Tawatchai Holmes (Andy), Zhedong Cao (Barry)
     @date:      15 OCT 2020
     @Descr:     Main game file 
+    
+The game is similar to the normal Rock, Paper and Scissor, but instead
+of 3 options we got 5 options to choose from between the player
+
+-Sword
+-Shield
+-Human
+-Gun
+-Grenade
+
+
+Each options consist of 2 wins and 2 loss
+
+-Sword beats Grenade and Shield, but lose to Gun and Human
+-Shield beats Gun and Human, but lose to Sword and Grenade
+-Human beats Gun and Sword, but lose to Shield and Grenade
+-Gun beats Grenade and Sword, but lose to Shield and Human
+-Grenade beats Human and Shield, but lose to Sword and Gun
+
 */
 
 #include "system.h"
