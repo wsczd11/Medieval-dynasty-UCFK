11840f534985f90bbca11679cde3bd7aa6f966c5
Andy Holmes <tho78@uclive.ac.nz>
2020-10-11 20:14:32 +1300

    Complete speaker.c

--- speaker.c
+++ speaker.c
@@ -1,24 +1,25 @@
-/** @file   beep1.c
-    @author Tawatchai Holmes (Andy), Zhedong Cao (Barry)
-    @date   15 OCT 2020
-    @Descr:  Makefile for game
+/** @file:       speaker.c
+    @author:     Tawatchai Holmes (Andy), Zhedong Cao (Barry)
+    @date:       15 OCT 2020
+    @Descr:      Speaker function used in game.c to give the speaker sound
 */
 
 #include "system.h"
 #include "pio.h"
 #include "pacer.h"
 
-/* Connect piezo tweeter to pins 6 and 8 of UCFK4 P1 connector
-   for push-pull operation.  */
 #define PIEZO1_PIO PIO_DEFINE (PORT_D, 4)
 #define PIEZO2_PIO PIO_DEFINE (PORT_D, 6)
 
+/* Initialise speaker */
 void init_speaker(void)
 {
     pio_config_set (PIEZO1_PIO, PIO_OUTPUT_LOW);
     pio_config_set (PIEZO2_PIO, PIO_OUTPUT_HIGH);
 }
 
+
+/* Output sound from speaker */
 void use_speaker()
 {
     pio_output_toggle (PIEZO1_PIO);
