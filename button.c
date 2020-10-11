3af89ef8a47d50a118e54801050975263626810b
Andy Holmes <tho78@uclive.ac.nz>
2020-10-11 20:23:30 +1300

    button.c

--- button.c
+++ button.c
@@ -7,14 +7,14 @@
 #include "pio.h"
 #include "button.h"
 
-/* Return non-zero if button pressed.  */
+/* Return non-zero if button pressed. */
 int button_pressed_p (void)
 {
     return pio_input_get(PD7_PIO);
 }
 
 
-/* Initialise button1.  */
+/* Initialise button1. */
 void button_init (void)
 {
     pio_config_set(PD7_PIO, PIO_INPUT);
