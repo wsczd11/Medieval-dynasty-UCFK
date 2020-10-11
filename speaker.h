f5e136cdbf1dbb5405b1094731bbdf7b1e2c3eb4
Andy Holmes <tho78@uclive.ac.nz>
2020-10-11 22:27:18 +1300

    speaker.h

--- speaker.h
+++ speaker.h
@@ -1,4 +1,4 @@
-/** @file:      beep1.c
+/** @file:      speaker.h
     @author:    Tawatchai Holmes (Andy) tho78, Zhedong Cao (Barry) zca19
     @date:      15 OCT 2020
     @Descr:     Speaker function in .h format
