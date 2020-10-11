360933d5683b6941e20e00c82672480bdc2d58f2
Andy Holmes <tho78@uclive.ac.nz>
2020-10-11 20:22:36 +1300

    led.c

--- led.c
+++ led.c
@@ -7,21 +7,21 @@
 #include "pio.h"
 #include "led.h"
 
-/* Turn LED on.  */
+/* Turn LED on. */
 void led_on (void)
 {
     pio_output_high(PC2_PIO);
 }
 
 
-/* Turn LED off.  */
+/* Turn LED off. */
 void led_off (void)
 {
     pio_output_low(PC2_PIO);
 }
 
 
-/* Initialise LED.  */
+/* Initialise LED. */
 void led_init (void)
 {
     pio_config_set(PC2_PIO, PIO_OUTPUT_HIGH);
