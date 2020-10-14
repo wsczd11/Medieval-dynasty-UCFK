d1987c10107f68b72542d01f6db7937031c0858d
Andy Holmes <tho78@uclive.ac.nz>
2020-10-14 15:48:12 +1300

    led.h

--- led.h
+++ led.h
@@ -1,21 +1,28 @@
 /** @file:      led.h
     @author:    Tawatchai Holmes (Andy) tho78, Zhedong Cao (Barry) zca19
     @date:      15 OCT 2020
-    @Descr:     Led functions in .h format
+    @brief:     Led functions in .h format
 */
 
 #ifndef LED_H
 #define LED_H
 
-/* Turn LED on.  */
+
+/**
+ * @brief: Turn LED on
+*/
 void led_on (void);
 
 
-/* Turn LED off.  */
+/**
+ * @brief: Turn LED off
+*/
 void led_off (void);
 
 
-/* Initialise LED.  */
+/**
+ * @brief: Initialise LED
+*/
 void led_init (void);
 
 #endif
