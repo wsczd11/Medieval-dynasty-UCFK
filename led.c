8795e8cf3d8dda025701dc491b625d6d82a86139
Andy Holmes <tho78@uclive.ac.nz>
2020-10-14 15:47:32 +1300

    led.c

--- led.c
+++ led.c
@@ -1,27 +1,34 @@
 /** @file:      led.c
     @author:    Tawatchai Holmes (Andy) tho78, Zhedong Cao (Barry) zca19
     @date:      15 OCT 2020
-    @Descr:     Led functions use in game.c
+    @brief:     Led functions use in game.c to turn on and off the LED
 */
 
 #include "pio.h"
 #include "led.h"
 
-/* Turn LED on. */
+
+/**
+ * @brief: Turn LED on
+*/
 void led_on (void)
 {
     pio_output_high(PC2_PIO);
 }
 
 
-/* Turn LED off. */
+/**
+ * @brief: Turn LED off
+*/
 void led_off (void)
 {
     pio_output_low(PC2_PIO);
 }
 
 
-/* Initialise LED. */
+/**
+ * @brief: Initialise LED
+*/
 void led_init (void)
 {
     pio_config_set(PC2_PIO, PIO_OUTPUT_HIGH);
