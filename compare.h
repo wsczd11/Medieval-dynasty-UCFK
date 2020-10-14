529bf9ed0d42fdb87142b9245c2301e9aacab7af
Andy Holmes <tho78@uclive.ac.nz>
2020-10-14 15:24:28 +1300

    compare.h

--- compare.h
+++ compare.h
@@ -1,13 +1,21 @@
 /** @file:      compare.h
     @author:    Tawatchai Holmes (Andy) tho78, Zhedong Cao (Barry) zca19
     @date:      15 OCT 2020
-    @Descr:     Compare functions in .h format
+    @brief:     compare functions use in a .h format
 */
+
 
 #ifndef COMPARE_H
 #define COMPARE_H
 
-/* compare two choice.  */
+
+/**
+ * @brief: Comparing the 2 different choices from each of the player
+ * @param: Player_1 choice
+ * @param: Player_2 choice
+ * @return: Return 0 if player1 wins, 1 otherwise.
+*/
+
 int compare_choice(uint8_t Player_1, uint8_t Player_2);
 
 
